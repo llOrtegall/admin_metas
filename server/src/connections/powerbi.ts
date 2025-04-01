d5b660bd32e2a82b03ab69ec5a09c3ed209927dd
llOrtegall <IvanOrtega_97@hotmail.com>
2025-04-01 10:46:17 -0500

    add environment schema validation and PowerBI database connection setup

---
+++ server/src/connections/powerbi.ts
@@ -0,0 +1,10 @@
+import { DB_POWERBI_DATABASE, DB_POWERBI_HOST, DB_POWERBI_PASS, DB_POWERBI_PORT, DB_POWERBI_USER } from '../config/envSchema';
+import { Sequelize } from 'sequelize';
+
+export const PowerBI = new Sequelize(DB_POWERBI_DATABASE, DB_POWERBI_USER, DB_POWERBI_PASS, {
+  host: DB_POWERBI_HOST,
+  port: DB_POWERBI_PORT,
+  dialect: 'mysql',
+  timezone: '-00:00',
+  logging: false
+});
