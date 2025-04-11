24a83495edd9022aeec6bd4c4d27834aab9036be
llOrtegall <IvanOrtega_97@hotmail.com>
2025-04-11 09:12:07 -0500

    agregando componente dialog

---
+++ client/src/app/sucursales/editsucursal.tsx
@@ -0,0 +1,15 @@
+import { DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+
+export default function EditSucursal() {
+  return (
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>Are you absolutely sure?</DialogTitle>
+        <DialogDescription>
+          This action cannot be undone. This will permanently delete your account
+          and remove your data from our servers.
+        </DialogDescription>
+      </DialogHeader>
+    </DialogContent >
+  )
+}
