c05411f4f7c6ea41062d9f79b22d71b7452c134d
llOrtegall <IvanOrtega_97@hotmail.com>
2025-04-11 09:12:18 -0500

    agregando dialog

--- client/src/app/sucursales/info-sucursal.tsx
+++ client/src/app/sucursales/info-sucursal.tsx
@@ -1,9 +1,12 @@
+import { Dialog, DialogTrigger } from "@/components/ui/dialog"
+
 import { SucursalInfo } from '@/types/Sucursales';
 import { URL_API_DATA } from '@/utils/constants';
 import { Card } from '@/components/ui/card';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import axios from 'axios';
+import EditSucursal from "./editsucursal";
 
 export default function InfoSucursal() {
   const param = useParams();
@@ -24,41 +27,39 @@ export default function InfoSucursal() {
     <Card className='px-2'>
       <h1 className="text-xl font-bold px-6 py-2 text-center">Información Detallada Sucursal - N° {data?.CODIGO}</h1>
 
-      <section className="px-6 flex justify-around ">
+      <section className="px-6 grid grid-cols-3 gap-2">
+        <p>
+          <strong>Nombre:</strong> {data?.NOMBRE}
+        </p>
+        <p>
+          <strong>C. Costo:</strong> {data?.CCOSTO}
+        </p>
+        <p>
+          <strong>Dirección:</strong> {data?.DIRECCION}
+        </p>
+        <p>
+          <strong>Tipo:</strong> {data?.TIPO}
+        </p>
+        <p>
+          <strong>Supervisor:</strong> {data?.SUPERVISOR}
+        </p>
 
-        <article className='flex flex-col space-y-2'>
-          <p>
-            <strong>C. Costo:</strong> {data?.CCOSTO}
-          </p>
-          <p>
-            <strong>Nombre:</strong> {data?.NOMBRE}
-          </p>
-          <p>
-            <strong>Dirección:</strong> {data?.DIRECCION}
-          </p>
-          <p>
-            <strong>Tipo:</strong> {data?.TIPO}
-          </p>
-          <p>
-            <strong>Supervisor:</strong> {data?.SUPERVISOR}
-          </p>
-        </article>
-
-        <article className='flex flex-col space-y-2'>
-          <p>
-            <strong>Célula:</strong> {data?.CELULA}
-          </p>
-          <p>
-            <strong>Subzona:</strong> {data?.SUBZONA}
-          </p>
-          <p>
-            <strong>Canal:</strong> {data?.CANAL}
-          </p>
-          <p>
-            <strong>Estado:</strong> {data?.ESTADO === 'A' ? 'Activo' : 'Inactivo'}
-          </p>
-        </article>
-
+        <p>
+          <strong>Célula:</strong> {data?.CELULA}
+        </p>
+        <p>
+          <strong>Subzona:</strong> {data?.SUBZONA}
+        </p>
+        <p>
+          <strong>Canal:</strong> {data?.CANAL}
+        </p>
+        <p>
+          <strong>Estado:</strong> {data?.ESTADO === 'A' ? 'Activo' : 'Inactivo'}
+        </p>
+        <Dialog>
+          <DialogTrigger className="mx-32 cursor-pointer border rounded-md py-1.5 hover:bg-gray-100">Actualizar Información</DialogTrigger>
+          <EditSucursal />
+        </Dialog>
       </section>
 
 
