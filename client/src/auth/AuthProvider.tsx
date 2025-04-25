b3b466cc781364ab5615275942626dfb44cab6e1
llOrtegall <IvanOrtega_97@hotmail.com>
2025-04-25 10:56:48 -0500

    modify auth conxtext and provider on app

--- client/src/auth/AuthProvider.tsx
+++ client/src/auth/AuthProvider.tsx
@@ -1,63 +1,29 @@
 import { createContext, useContext, useEffect, useState, ReactNode } from 'react'
-import { URL_API_LOGIN, APP_NAME } from '@/utils/constants'
+import { URL_API_LOGIN } from '@/utils/constants'
 import { IAuthContext } from '@/types/Interfaces'
 import { type User } from '@/types/User'
 import axios from 'axios'
 
-// Crear el contexto de autenticación
 const AuthContext = createContext<IAuthContext | undefined>(undefined)
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [user, setUser] = useState<User | null>(null)
   const [empresa, setEmpresa] = useState<string>('Multired')
-  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false)
 
-  // Función para validar la cookie de sesión
-  const validateCookie = (): boolean => {
-    const cookie = document.cookie
-      .split('; ')
-      .find(row => row.startsWith(`${APP_NAME}=`))
-    return !!cookie
-  }
-
-  // Función para obtener el perfil del usuario
-  const fetchUserProfile = async () => {
-    try {
-      const response = await axios.get(`${URL_API_LOGIN}/profile`, { params: { app: APP_NAME } })
-      if (response.status === 200) {
-        setIsAuthenticated(true)
-        setUser(response.data)
-      }
-    } catch (error) {
-      console.log(error);
-  
-      /*
-      if (error.response?.status === 401) {
-        setIsAuthenticated(false)
-        setUser(null)
-      } else {
-        console.error('Error al obtener el perfil del usuario:', error)
-      }
-        */
-    }
-  }
-
-  // Efecto para validar la sesión al cargar la aplicación
   useEffect(() => {
-    const initializeAuth = async () => {
-      if (!validateCookie()) {
-        setIsAuthenticated(false)
-        setUser(null)
-        return
-      }
-      await fetchUserProfile()
-    }
-
-    initializeAuth()
-  }, []) // Dependencia vacía para ejecutar solo una vez al montar el componente
+    axios.get(`${URL_API_LOGIN}/profile`)
+     .then( res => {
+        if (res.status === 200) {
+          setUser(res.data)
+        } else {
+          setUser(null)
+        }
+     })
+  }, [isAuthenticated])
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated, user, setUser, empresa, setEmpresa }}>
+    <AuthContext.Provider value={{ user, setUser, empresa, setEmpresa, isAuthenticated, setIsAuthenticated }}>
       {children}
     </AuthContext.Provider>
   )
